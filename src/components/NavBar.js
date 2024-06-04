ceaff4708f319ed849c304c872d34a2177d6ff90
Abraham Scott <abrahamscott02@gmail.com>
2024-06-04 16:19:59 -0600

    Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,27 @@ import "./NavBar.css";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-    
+    <nav className="navbar" role="navigation">
+      <NavLink 
+        to="/" 
+        className="nav-link"
+      >
+        Home
+      </NavLink>
+      <NavLink 
+        to="/directors" 
+        className="nav-link"
+      >
+        Directors
+      </NavLink>
+      <NavLink 
+        to="/actors" 
+        className="nav-link"
+      >
+        Actors
+      </NavLink>
     </nav>
-    );
+  );
 };
 
 export default NavBar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,30 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
 
-function Home() {
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // Replace with your actual data fetching logic
+    fetch("http://localhost:4000/movies")
+      .then(response => response.json())
+      .then(data => setMovies(data))
+      .catch(error => console.error("Error fetching movies:", error));
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Info goes here! */}
+        <h1>Home Page</h1>
+        <div>
+          {movies.map(movie => (
+            <MovieCard key={movie.id} id={movie.id} title={movie.title} info={movie.info} />
+          ))}
+        </div>
       </main>
     </>
   );
